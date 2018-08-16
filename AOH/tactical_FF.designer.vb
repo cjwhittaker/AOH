c5c7c7fd959b73f4b5ccefc0e5edb4bc5fd4d144
cjwhittaker <30289411+cjwhittaker@users.noreply.github.com>
2018-08-16 14:33:42 +0100

    Tactical Added

---
+++ AOH/tactical_FF.designer.vb
@@ -0,0 +1,274 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class tactical_FF
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(tactical_FF))
+        Me.getresult = New System.Windows.Forms.Button()
+        Me.Player = New System.Windows.Forms.Label()
+        Me.nextphase = New System.Windows.Forms.Button()
+        Me.Reset = New System.Windows.Forms.Button()
+        Me.exceptional = New System.Windows.Forms.CheckBox()
+        Me.Column = New System.Windows.Forms.CheckBox()
+        Me.fresh = New System.Windows.Forms.CheckBox()
+        Me.spent = New System.Windows.Forms.CheckBox()
+        Me.disordered = New System.Windows.Forms.CheckBox()
+        Me.Brigade = New System.Windows.Forms.CheckBox()
+        Me.attached = New System.Windows.Forms.CheckBox()
+        Me.detached = New System.Windows.Forms.CheckBox()
+        Me.adjust = New System.Windows.Forms.Button()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.doctrine = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'getresult
+        '
+        Me.getresult.Font = New System.Drawing.Font("Calibri", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.getresult.Location = New System.Drawing.Point(240, 510)
+        Me.getresult.Name = "getresult"
+        Me.getresult.Size = New System.Drawing.Size(210, 47)
+        Me.getresult.TabIndex = 8
+        Me.getresult.Text = "Check "
+        Me.getresult.UseVisualStyleBackColor = True
+        '
+        'Player
+        '
+        Me.Player.BackColor = System.Drawing.Color.Transparent
+        Me.Player.Font = New System.Drawing.Font("Calibri", 27.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Player.Location = New System.Drawing.Point(142, 9)
+        Me.Player.MinimumSize = New System.Drawing.Size(300, 0)
+        Me.Player.Name = "Player"
+        Me.Player.Size = New System.Drawing.Size(400, 70)
+        Me.Player.TabIndex = 9
+        Me.Player.Text = "Label1"
+        Me.Player.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'nextphase
+        '
+        Me.nextphase.Font = New System.Drawing.Font("Calibri", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.nextphase.Location = New System.Drawing.Point(550, 603)
+        Me.nextphase.Name = "nextphase"
+        Me.nextphase.Size = New System.Drawing.Size(118, 45)
+        Me.nextphase.TabIndex = 10
+        Me.nextphase.Text = "Next Phase"
+        Me.nextphase.UseVisualStyleBackColor = True
+        '
+        'Reset
+        '
+        Me.Reset.Font = New System.Drawing.Font("Calibri", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Reset.Location = New System.Drawing.Point(434, 603)
+        Me.Reset.Name = "Reset"
+        Me.Reset.Size = New System.Drawing.Size(109, 45)
+        Me.Reset.TabIndex = 11
+        Me.Reset.Text = "Reset Form"
+        Me.Reset.UseVisualStyleBackColor = True
+        '
+        'exceptional
+        '
+        Me.exceptional.Appearance = System.Windows.Forms.Appearance.Button
+        Me.exceptional.Font = New System.Drawing.Font("Calibri", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.exceptional.Location = New System.Drawing.Point(142, 100)
+        Me.exceptional.Name = "exceptional"
+        Me.exceptional.Size = New System.Drawing.Size(400, 40)
+        Me.exceptional.TabIndex = 12
+        Me.exceptional.Text = "Exceptional Corps or Divisional Leader"
+        Me.exceptional.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        Me.exceptional.UseVisualStyleBackColor = True
+        '
+        'Column
+        '
+        Me.Column.Appearance = System.Windows.Forms.Appearance.Button
+        Me.Column.Font = New System.Drawing.Font("Calibri", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Column.Location = New System.Drawing.Point(142, 300)
+        Me.Column.Name = "Column"
+        Me.Column.Size = New System.Drawing.Size(400, 40)
+        Me.Column.TabIndex = 13
+        Me.Column.Text = "Column Formation"
+        Me.Column.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        Me.Column.UseVisualStyleBackColor = True
+        '
+        'fresh
+        '
+        Me.fresh.Appearance = System.Windows.Forms.Appearance.Button
+        Me.fresh.Font = New System.Drawing.Font("Calibri", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.fresh.Location = New System.Drawing.Point(142, 350)
+        Me.fresh.Name = "fresh"
+        Me.fresh.Size = New System.Drawing.Size(400, 40)
+        Me.fresh.TabIndex = 14
+        Me.fresh.Text = "Fresh"
+        Me.fresh.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        Me.fresh.UseVisualStyleBackColor = True
+        '
+        'spent
+        '
+        Me.spent.Appearance = System.Windows.Forms.Appearance.Button
+        Me.spent.Font = New System.Drawing.Font("Calibri", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.spent.Location = New System.Drawing.Point(142, 400)
+        Me.spent.Name = "spent"
+        Me.spent.Size = New System.Drawing.Size(400, 40)
+        Me.spent.TabIndex = 15
+        Me.spent.Text = "Spent"
+        Me.spent.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        Me.spent.UseVisualStyleBackColor = True
+        '
+        'disordered
+        '
+        Me.disordered.Appearance = System.Windows.Forms.Appearance.Button
+        Me.disordered.Font = New System.Drawing.Font("Calibri", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.disordered.Location = New System.Drawing.Point(142, 450)
+        Me.disordered.Name = "disordered"
+        Me.disordered.Size = New System.Drawing.Size(400, 40)
+        Me.disordered.TabIndex = 16
+        Me.disordered.Text = "Disordered"
+        Me.disordered.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        Me.disordered.UseVisualStyleBackColor = True
+        '
+        'Brigade
+        '
+        Me.Brigade.Appearance = System.Windows.Forms.Appearance.Button
+        Me.Brigade.Font = New System.Drawing.Font("Calibri", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Brigade.Location = New System.Drawing.Point(142, 250)
+        Me.Brigade.Name = "Brigade"
+        Me.Brigade.Size = New System.Drawing.Size(400, 40)
+        Me.Brigade.TabIndex = 17
+        Me.Brigade.Text = "Exceptional Brigade Commander"
+        Me.Brigade.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        Me.Brigade.UseVisualStyleBackColor = True
+        '
+        'attached
+        '
+        Me.attached.Appearance = System.Windows.Forms.Appearance.Button
+        Me.attached.Font = New System.Drawing.Font("Calibri", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.attached.Location = New System.Drawing.Point(142, 200)
+        Me.attached.Name = "attached"
+        Me.attached.Size = New System.Drawing.Size(400, 40)
+        Me.attached.TabIndex = 18
+        Me.attached.Text = "Attached Corps or Divisonal leader"
+        Me.attached.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        Me.attached.UseVisualStyleBackColor = True
+        '
+        'detached
+        '
+        Me.detached.Appearance = System.Windows.Forms.Appearance.Button
+        Me.detached.Font = New System.Drawing.Font("Calibri", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.detached.Location = New System.Drawing.Point(142, 150)
+        Me.detached.Name = "detached"
+        Me.detached.Size = New System.Drawing.Size(400, 40)
+        Me.detached.TabIndex = 19
+        Me.detached.Text = "Detached Corps or Divisonal leader"
+        Me.detached.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        Me.detached.UseVisualStyleBackColor = True
+        '
+        'adjust
+        '
+        Me.adjust.Font = New System.Drawing.Font("Calibri", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.adjust.Location = New System.Drawing.Point(325, 603)
+        Me.adjust.Name = "adjust"
+        Me.adjust.Size = New System.Drawing.Size(102, 45)
+        Me.adjust.TabIndex = 20
+        Me.adjust.Text = "Adjust Cas"
+        Me.adjust.UseVisualStyleBackColor = True
+        '
+        'Button1
+        '
+        Me.Button1.Font = New System.Drawing.Font("Calibri", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.Location = New System.Drawing.Point(66, 603)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(157, 45)
+        Me.Button1.TabIndex = 21
+        Me.Button1.Text = "Movement Rates"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Font = New System.Drawing.Font("Calibri", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button2.Location = New System.Drawing.Point(12, 603)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(47, 45)
+        Me.Button2.TabIndex = 22
+        Me.Button2.Text = "<"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'doctrine
+        '
+        Me.doctrine.Font = New System.Drawing.Font("Calibri", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.doctrine.Location = New System.Drawing.Point(230, 603)
+        Me.doctrine.Name = "doctrine"
+        Me.doctrine.Size = New System.Drawing.Size(88, 45)
+        Me.doctrine.TabIndex = 23
+        Me.doctrine.Text = "Doctrine"
+        Me.doctrine.UseVisualStyleBackColor = True
+        Me.doctrine.Visible = False
+        '
+        'tactical_FF
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackgroundImage = Global.AOH.My.Resources.Resources.flags01
+        Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.ClientSize = New System.Drawing.Size(684, 661)
+        Me.Controls.Add(Me.doctrine)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.adjust)
+        Me.Controls.Add(Me.detached)
+        Me.Controls.Add(Me.attached)
+        Me.Controls.Add(Me.Brigade)
+        Me.Controls.Add(Me.disordered)
+        Me.Controls.Add(Me.spent)
+        Me.Controls.Add(Me.fresh)
+        Me.Controls.Add(Me.Column)
+        Me.Controls.Add(Me.exceptional)
+        Me.Controls.Add(Me.Reset)
+        Me.Controls.Add(Me.nextphase)
+        Me.Controls.Add(Me.Player)
+        Me.Controls.Add(Me.getresult)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
+        Me.MaximumSize = New System.Drawing.Size(700, 700)
+        Me.MinimizeBox = False
+        Me.MinimumSize = New System.Drawing.Size(700, 700)
+        Me.Name = "tactical_FF"
+        Me.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Tactical March "
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents getresult As System.Windows.Forms.Button
+    Friend WithEvents Player As System.Windows.Forms.Label
+    Friend WithEvents nextphase As System.Windows.Forms.Button
+    Friend WithEvents Reset As System.Windows.Forms.Button
+    Friend WithEvents exceptional As System.Windows.Forms.CheckBox
+    Friend WithEvents Column As System.Windows.Forms.CheckBox
+    Friend WithEvents fresh As System.Windows.Forms.CheckBox
+    Friend WithEvents spent As System.Windows.Forms.CheckBox
+    Friend WithEvents disordered As System.Windows.Forms.CheckBox
+    Friend WithEvents Brigade As System.Windows.Forms.CheckBox
+    Friend WithEvents attached As System.Windows.Forms.CheckBox
+    Friend WithEvents detached As System.Windows.Forms.CheckBox
+    Friend WithEvents adjust As System.Windows.Forms.Button
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents doctrine As Button
+End Class
---
+++ AOH/tactical_FF.vb
@@ -0,0 +1,145 @@
+﻿Public Class tactical_FF
+    Public runaway As Integer
+    Private Sub Reset_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Reset.Click
+        resetform()
+    End Sub
+    Private Sub resetform()
+        exceptional.Checked = False
+        detached.Checked = False
+        attached.Checked = False
+        Brigade.Checked = False
+        Column.Checked = False
+        fresh.Checked = False
+        spent.Checked = False
+        disordered.Checked = False
+    End Sub
+    Private Sub toggle_checks(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles attached.CheckedChanged, detached.CheckedChanged, spent.CheckedChanged, fresh.CheckedChanged
+        If Not sender.checked Then Exit Sub
+        If sender.name = "attached" Then
+            detached.Checked = False
+        ElseIf sender.name = "detached" Then
+            attached.Checked = False
+        ElseIf sender.name = "fresh" Then
+            spent.Checked = False
+        ElseIf sender.name = "spent" Then
+            fresh.Checked = False
+        Else
+        End If
+    End Sub
+    Private Sub check_color(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles _
+        attached.CheckedChanged, detached.CheckedChanged, spent.CheckedChanged, fresh.CheckedChanged,
+        disordered.CheckedChanged, exceptional.CheckedChanged, Brigade.CheckedChanged, Column.CheckedChanged
+        If sender.checked Then sender.backcolor = Color.Goldenrod Else sender.backcolor = Control.DefaultBackColor
+
+    End Sub
+
+    Private Sub getresult_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles getresult.Click
+        Dim quitfield As Boolean, modifier As Integer, result As Integer, msg As String, broken As Boolean
+        modifier = 0 : quitfield = False : broken = False
+        If exceptional.Checked Then modifier = modifier + 1
+        If attached.Checked Then modifier = modifier + 2
+        If detached.Checked Then modifier = modifier + 1
+        If Brigade.Checked Then modifier = modifier + 1
+        If Column.Checked Then modifier = modifier + 1
+        If fresh.Checked Then modifier = modifier + 2
+        If spent.Checked Then modifier = modifier - 2
+        modifier = modifier + get_cohesion(Me.Tag)
+        Dim d As Integer = dice(10)
+        result = d + modifier
+        Dim droll As String = "[" + Str(d) + " + " + Str(modifier) + "]" + vbNewLine
+        If Not display_dice Then droll = ""
+
+        If disordered.Checked Then
+            If result <= 0 Then
+                msg = "Quits the Field" + vbNewLine + "The entire Brigade skedaddles"
+                quitfield = True
+            ElseIf result <= 2 Then
+                msg = "Broken" + vbNewLine + "Retreat a full move disordered and one stand skedaddles"
+                broken = True
+            ElseIf result <= 4 Then
+                msg = "Wavering" + vbNewLine + "Hold ground in disorder"
+            ElseIf result <= 6 Then
+                msg = "Shaken" + vbNewLine + "Rallies and holds ground"
+            ElseIf result <= 9 Then
+                msg = "Rally" + vbNewLine + "Rallies and may move half but cannot change formation"
+            Else
+                msg = "Rally with Elan" + vbNewLine + "Rallies and may move normally and can change formation"
+            End If
+        Else
+            If result <= 0 Then
+                msg = "Retire" + vbNewLine + "Brigade retreats a full move"
+            ElseIf result <= 2 Then
+                msg = "Disengage" + vbNewLine + "Brigade retreats out of musket range"
+            ElseIf result <= 4 Then
+                msg = "Hold Ground" + vbNewLine + "Cannot move or change formation"
+            ElseIf result <= 6 Then
+                msg = "Tardy" + vbNewLine + "May move half but cannot change formation"
+            Else
+                msg = "Well Handled" + vbNewLine + "May move normally and can change formation"
+            End If
+
+
+        End If
+        resultform.result.Text = droll + msg
+        resultform.ShowDialog()
+        If quitfield Or broken Then
+            If quitfield Then
+                calculator.Tag = "rout"
+                calculator.ShowDialog()
+                calculator.Tag = ""
+            Else
+                runaway = 1
+            End If
+            If Me.Tag = scenariodefaults.player1.Text Then
+                casualties.p1_ske.Value = casualties.p1_ske.Value + runaway
+            Else
+                casualties.p2_ske.Value = casualties.p2_ske.Value + runaway
+            End If
+        End If
+
+        resetform()
+    End Sub
+
+    Private Sub Tactical_Activated(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Activated
+        resetform()
+        If generals Is Nothing Then doctrine.Enabled = False
+    End Sub
+
+
+    Private Sub adjust_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles adjust.Click
+        casualties.ShowDialog()
+    End Sub
+
+    Private Sub nextphase_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles nextphase.Click
+        If movement_FF.Visible Then movement_FF.Hide()
+        scenariodefaults.quit = False
+        Me.Hide()
+    End Sub
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        movement_FF.Show()
+    End Sub
+
+    Private Sub Manouvre_FormClosing(ByVal sender As Object, ByVal e As System.Windows.Forms.FormClosingEventArgs) Handles Me.FormClosing
+        If scenariodefaults.quit Then quitprogram()
+    End Sub
+
+
+    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
+        scenariodefaults.quit = False
+        goback = True
+        Me.Hide()
+    End Sub
+
+    Private Sub doctrine_Click(sender As Object, e As EventArgs) Handles doctrine.Click
+        With resultform
+            .adjust.Visible = False
+            .reverse.Visible = False
+            .Text = "Battle Doctrine "
+            .result.Text = "Battle Doctrine" + vbNewLine + Me.Tag + " CinC has adopted " + generals(Me.Tag + "CinC").plan + vbNewLine + "The Order of March chosen is " + generals(Me.Tag + "CinC").oom + Replace(generals(Me.Tag + "CinC").doctrine, "!", vbNewLine)
+            .ShowDialog()
+            .reverse.Visible = True
+            .adjust.Visible = True
+        End With
+    End Sub
+End Class
